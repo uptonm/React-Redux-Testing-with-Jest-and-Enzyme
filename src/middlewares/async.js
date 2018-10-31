a0679932fb46583c6ac4e8f93d058f2de6339ff1
Mike Upton <uptonm@wit.edu>
2018-10-31 12:38:19 -0400

    Comments for readability

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,9 +1,13 @@
 export default ({ dispatch }) => next => action => {
-  debugger;
+  // Check to see if the action has a promise in the payload property
+  // If it does wait for it to resolve.
+  // If not, semd tp the next middleware
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
 
+  // Wait for the promise to resolve then create a new action with
+  // Its data payload, and dispatch it to the reducer
   action.payload.then(function(response) {
     const newAction = { ...action, payload: response };
     dispatch(newAction);
