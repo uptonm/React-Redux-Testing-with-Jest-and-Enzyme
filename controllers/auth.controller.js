43169626a0db96266a5fb5a9970720e23d03a7ac
Mike Upton <uptonm@wit.edu>
2018-11-02 15:24:38 -0400

    Passwords now encrypt on save

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,14 +2,22 @@ const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
 exports.signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide an email address and a password" });
+  }
   // See if a user with a given email exists
   const exists = await User.findOne({ email: req.body.email });
   if (exists) {
+    // If they exist return an error 422
     return res.status(422).send({ error: "User already exists" });
   }
+  // If they dont create a user record
   const user = await new User({
     email: req.body.email,
     password: req.body.password
   }).save();
-  return res.status(200).send(user);
+  // Return a status that the user was created
+  return res.status(200).send({ success: true });
 };
--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   first: String,
   last: String,
-  email: { type: String, lowercase: true },
+  email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On save hook encrypt password
+userSchema.pre("save", function(next) {
+  const user = this;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 const User = mongoose.model("users", userSchema);
